7e5683efe581a1ada8d1c2c9db6a7f13823cee22
Zoltán Korándi <korandi.z@gmail.com>
2013-10-10 04:06:50 +0200

    chart lib v 0.0.0.0.1

---
+++ charts/chart.js
@@ -0,0 +1,644 @@
+statsd.config.httpApi = "http://kozo.dyndns.info:5999/";
+statsd.config.wsApi = "ws://kozo.dyndns.info:5999/";
+statsd.config.timeout = 1000;
+
+function Chart(svg, fetcherFactory, clockSkew) {
+	var	svg = d3.select(svg),
+		settings = {
+			width: 640,
+			height: 480,
+			backgroundColor: "#f8f8f8",
+			fontSize: 12,
+			fontFamily: "sans-serif",
+
+			granularity: 60e3,
+			xMax: null,
+			xCount: 60,
+			xTicks: 5,
+			xGridColor: "#888",
+			xLabelColor: "#000",
+			xLocked: false,
+			timeFormat1: "%m/%d/%y",
+			timeFormat2: "%I:%M %p",
+
+			yMin: 0,
+			yMax: null,
+			yTicks: 5,
+			yGridColor: "#888",
+			yLabelColor: "#000",
+			yLocked: false,
+
+			retry: 10e3,
+
+			data: [
+				{
+					label: "Test 0",
+					channel: "test0:counter",
+					autoY: true,
+					color: "red",
+					width: 2
+				},
+				{
+					label: "Test 1, counter",
+					channel: "test1:counter",
+					autoY: true,
+					color: "green",
+					width: 2
+				},
+				{
+					label: "Test 1, gauge",
+					channel: "test1:gauge",
+					autoY: true,
+					color: "orange",
+					width: 2
+				},
+				{
+					label: "Test 2",
+					channel: "test2:avg",
+					autoY: true,
+					color: "lightblue",
+					width: 10
+				},
+				{
+					label: "Test 2 (cnt)",
+					channel: "test2:avg",
+					autoY: true,
+					color: "gray",
+					width: 2
+				}
+			]
+		},
+		margin = {t: 0, r: 0, b: 30, l: 50},
+		width,
+		height,
+		xScale = d3.time.scale(),
+		yScale = d3.scale.linear(),
+		fetcher,
+		fetcherN = 0,
+		values,
+		ts,
+		retryTimer = null,
+		xAnchor = null,
+		yAnchor = null,
+		xChanged = false,
+		yChanged = false,
+		stopWatching = null;
+
+	createElements();
+	showError(null);
+	updateSize();
+	setBackgroundColor();
+	resetXDomain();
+	redrawXGrid();
+	redrawYGrid();
+	resetData();
+	reload();
+
+	function createElements() {
+		var clipId = "clip-" + (+now()) + "-" + Math.ceil(Math.random() * 1e9);
+
+		svg.style("font-size", settings.fontSize + "px")
+			.style("font-family", settings.fontFamily);
+
+		svg.append("rect")
+			.classed("background", true)
+			.style("shape-rendering", "crispEdges");
+
+		svg.append("clipPath")
+			.classed("clipPath", true)
+			.attr("id", clipId)
+			.append("rect");
+
+		svg.append("g")
+			.classed("grid", true);
+
+		svg.append("g")
+			.classed("lines", true)
+			.attr("clip-path", "url(#" + clipId + ")");
+
+		svg.append("rect")
+			.classed("mouse", true)
+			.style("fill-opacity", 0)
+			.on("mousedown", xMousedown)
+			.on("mouseup", xMouseup)
+			.on("mousemove", xMousemove)
+			.on("mouseout", xMouseout)
+			.on("dblclick", dblclick);
+
+		svg.append("rect")
+			.classed("error", true)
+			.style("fill", "rgba(255, 128, 128, 0.75)")
+			.style("stroke", "#f00")
+			.style("shape-rendering", "crispEdges");
+
+		svg.append("text")
+			.classed("error", true)
+			.style("fill", "#000")
+			.style("text-anchor", "middle");
+
+		svg.append("clipPath")
+			.classed("yClipPath", true)
+			.attr("id", clipId + "-y")
+			.append("rect");
+			
+		svg.append("g")
+			.classed("yLabels", true)
+			.attr("clip-path", "url(#" + clipId + "-y)");
+
+		svg.append("rect")
+			.classed("yMouse", true)
+			.style("fill-opacity", 0)
+			.on("mousedown", yMousedown)
+			.on("mouseup", yMouseup)
+			.on("mousemove", yMousemove)
+			.on("mouseout", yMouseout)
+			.on("dblclick", yDblclick);
+
+		svg.append("clipPath")
+			.classed("xClipPath", true)
+			.attr("id", clipId + "-x")
+			.append("rect");
+
+		svg.append("g")
+			.classed("xLabels", true)
+			.attr("clip-path", "url(#" + clipId + "-x)");
+
+		svg.append("rect")
+			.classed("xMouse", true)
+			.style("fill-opacity", 0)
+			.on("mousedown", xMousedown)
+			.on("mouseup", xMouseup)
+			.on("mousemove", xMousemove)
+			.on("mouseout", xMouseout)
+			.on("dblclick", xDblclick);
+	}
+
+	function updateSize() {
+		var translate = "translate(" + margin.l + " " + margin.t + ")";
+
+		width = settings.width - margin.l - margin.r;
+		height = settings.height - margin.t - margin.b;
+		xScale.range([0.5, width - 0.5]);
+		yScale.range([height-0.5, 0.5]);
+
+		svg.attr("width", settings.width)
+			.attr("height", settings.height);
+
+		svg.selectAll(".background, .mouse")
+			.attr("transform", translate)
+			.attr("width", width)
+			.attr("height", height);
+
+		svg.select(".clipPath rect")
+			.attr("width", width)
+			.attr("height", height);
+	
+		svg.selectAll(".lines, .grid")
+			.attr("transform", translate);
+
+		svg.select("rect.error")
+			.attr("x", margin.l + settings.fontSize + 0.5)
+			.attr("y", margin.t + height - 3*settings.fontSize + 0.5)
+			.attr("width", width - 2*settings.fontSize - 1)
+			.attr("height", 2*settings.fontSize);
+
+		svg.select("text.error")
+			.attr("x", margin.l + width/2)
+			.attr("y", margin.t + height - 1.65*settings.fontSize + 0.5);
+
+		svg.select(".yClipPath rect")
+			.attr("y", margin.t + 1)
+			.attr("width", margin.l)
+			.attr("height", height - 1);
+
+		svg.select(".yMouse")
+			.attr("transform", "translate(0 " + margin.t + ")")
+			.attr("width", margin.l)
+			.attr("height", height);
+
+		svg.select(".xClipPath rect")
+			.attr("x", margin.l)
+			.attr("y", margin.t + height)
+			.attr("width", width)
+			.attr("height", margin.b);
+
+		svg.select(".xMouse")
+			.attr("transform", "translate(" + margin.l + " " + (margin.t + height) + ")")
+			.attr("width", width)
+			.attr("height", margin.b);
+	}
+
+	function setBackgroundColor() {
+		svg.select(".background")
+			.style("fill", settings.backgroundColor);
+	}
+
+	function showError(msg) {
+		if (retryTimer !== null) {
+			clearTimeout(retryTimer);
+		}
+
+		if (msg !== null) {
+			svg.select("text.error")
+				.text(msg);
+			svg.selectAll(".error")
+				.style("display", null);
+
+			retryTimer = setTimeout(reload, settings.retry);
+		} else {
+			svg.selectAll(".error")
+				.style("display", "none");
+		}
+	}
+
+	function resetXDomain() {
+		var n,
+			offset,
+			xMax,
+			xMin;
+
+		if (settings.xMax === null) {
+			n = now();
+			offset = n.getTimezoneOffset() * 60e3;
+			xMax = (+n) + offset;
+			xMax -= xMax%settings.granularity + offset;
+		} else {
+			xMax = settings.xMax;
+		}
+		xMin = xMax - settings.xCount*settings.granularity;
+		xScale.domain([xMin, xMax]);
+	}
+
+	function resetYDomain() {
+		var yMin = settings.yMin,
+			yMax = settings.yMax,
+			i, j;
+			
+		if (yMin === null) {
+			yMin = Number.POSITIVE_INFINITY;
+			for (i = 0; i < settings.data.length; i++) {
+				if (settings.data[i].autoY) {
+					for (j = 0; j < values[i].length; j++) {
+						if (values[i][j] < yMin) {
+							yMin = values[i][j];
+						}
+					}
+				}
+			}
+		}
+
+		if (yMax === null) {
+			yMax = Number.NEGATIVE_INFINITY;
+			for (i = 0; i < settings.data.length; i++) {
+				if (settings.data[i].autoY) {
+					for (j = 0; j < values[i].length; j++) {
+						if (values[i][j] > yMax) {
+							yMax = values[i][j];
+						}
+					}
+				}
+			}
+		}
+
+		if (isFinite(yMin) && isFinite(yMax)) {
+			if (yMin == yMax) {
+				if (yMin == 0) {
+					yMax = 1;
+				} else {
+					yMin /= 2;
+					yMax *= 1.5;
+				}
+			}
+
+			yScale.domain([yMin, yMax]);
+			if (settings.yMin == null || settings.yMax == null) {
+				yScale.nice();
+			}
+		}
+	}
+
+	function redrawLines() {
+		var lines = svg.select(".lines").selectAll("path").data(values),
+			line = d3.svg.line()
+				.x(function (d, i) {
+					return xScale(ts + (i + 0.5)*settings.granularity);
+				})
+			.y(yScale);
+
+		lines.enter().append("path");
+		lines.exit().remove();
+	
+		lines.attr("d", line)
+			.attr("stroke", function (d, i) {
+				return settings.data[i].color;
+			})
+			.attr("stroke-width", function (d, i) {
+				return settings.data[i].width;
+			})
+			.attr("fill", "none");
+	}
+
+	function redrawXGrid() {
+		var ticks = xScale.ticks(settings.xTicks),
+			grid = svg.select(".grid").selectAll(".xGrid").data(ticks),
+			labels1 = svg.select(".xLabels").selectAll("text.l1").data(ticks),
+			labels2 = svg.select(".xLabels").selectAll("text.l2").data(ticks);
+
+		grid.enter().append("line")
+			.classed("xGrid", true)
+			.style("stroke", settings.xGridColor)	
+			.style("shape-rendering", "crispEdges");
+		grid.exit().remove();
+		grid.attr("x1", roundX)
+			.attr("y1", 0)
+			.attr("x2", roundX)
+			.attr("y2", height);
+
+		labels1.exit().remove();
+		labels1.enter().append("text")
+			.classed("l1", true)
+			.style("text-anchor", "middle")
+			.style("fill", settings.xLabelColor);
+		labels1.text(d3.time.format(settings.timeFormat1))
+			.attr("x", function (d, i) { return margin.l + roundX(d, i); })
+			.attr("y", margin.t + height + 1.1*settings.fontSize);
+
+		labels2.exit().remove();
+		labels2.enter().append("text")
+			.classed("l2", true)
+			.style("text-anchor", "middle")
+			.style("fill", settings.xLabelColor);
+		labels2.text(d3.time.format(settings.timeFormat2))
+			.attr("x", function (d, i) { return margin.l + roundX(d, i); })
+			.attr("y", margin.t + height + 2.2*settings.fontSize);
+	}
+
+	function redrawYGrid() {
+		var ticks = yScale.ticks(settings.yTicks),
+			grid = svg.select(".grid").selectAll(".yGrid").data(ticks),
+			labels = svg.select(".yLabels").selectAll("text").data(ticks);
+
+		grid.exit().remove();
+		grid.enter().append("line")
+			.classed("yGrid", true)
+			.style("stroke", settings.yGridColor)
+			.style("shape-rendering", "crispEdges");
+		grid.attr("x1", 0)
+			.attr("y1", roundY)
+			.attr("x2", width)
+			.attr("y2", roundY)
+
+		labels.exit().remove();
+		labels.enter().append("text")
+			.style("text-anchor", "end")
+			.style("fill", settings.yLabelColor);
+		labels.text(formatNumber)
+			.attr("x", margin.l-2)
+			.attr("y", function (d, i) { return margin.t + roundY(d, i); });
+
+	}
+
+	function reload() {
+		var N = fetcherN,
+			offset = now().getTimezoneOffset() * 60e3,
+			domain = xScale.domain(),
+			from,
+			until,
+			length;
+
+		from = Math.floor(((+domain[0]) + offset)/settings.granularity - 0.5);
+		from = (from * settings.granularity) - offset;
+
+		until = Math.ceil(((+domain[1]) + offset)/settings.granularity - 0.5);
+		until = (until * settings.granularity) - offset;
+
+		length = (until - from) / settings.granularity + 1;
+
+		fetcher(from, length, function (err, d) {
+			if (fetcherN != N) {
+				return;
+			}
+	
+			showError(err);
+			if (err !== null) {
+				return;
+			}
+
+			setData(d);
+		});
+	}
+
+	function setData(d) {
+		values = d.data;
+		ts = d.ts;
+		if (!yChanged) {
+			resetYDomain();
+		}
+		redrawYGrid();
+		redrawLines();
+	}
+
+	function resetData() {
+		var channels = settings.data.map(function (e) { return e.channel; });
+		fetcher = fetcherFactory(channels, settings.granularity);
+		fetcherN++;
+		startWatching();
+		values = [];
+		ts = null;
+	}
+
+	function startWatching() {
+		var N = fetcherN,
+			offset = now().getTimezoneOffset() * 60e3;
+
+		if (stopWatching !== null) {
+			stopWatching();
+		}
+
+		stopWatching = fetcher.watch(offset, function (ts) {
+			var d;
+
+			if (fetcherN != N) {
+				return;
+			}
+
+			if (!xChanged) {
+				resetXDomain();
+				redrawXGrid();
+				redrawLines();
+			}
+
+			d = xScale.domain();
+			if (ts > d[0] && ts < d[1]) {
+				reload();
+			}
+		});
+	}
+
+	function xMousedown() {
+		if (settings.xLocked) {
+			return;
+		}
+
+		xAnchor = +xScale.invert(d3.mouse(this)[0]);
+		d3.event.preventDefault();
+	}
+
+	function xMouseup() {
+		xAnchor = null;
+	}
+
+	function xMousemove() {
+		var t = xScale.invert(d3.mouse(this)[0]),
+			d = xScale.domain();
+
+		if (xAnchor === null) {
+			return;
+		}
+
+		d = [+d[0], +d[1]];
+
+		if (d3.event.shiftKey) {
+			d[0] = d[1] - (d[1] - xAnchor)/(d[1] - t)*(d[1] - d[0]);
+		} else {
+			d[0] += xAnchor - t;
+			d[1] += xAnchor - t;
+		}
+		xScale.domain(d);
+		xChanged = true;
+		redrawXGrid();
+		redrawLines();
+		reload();
+	}
+
+	function xMouseout() {
+		var rtg = d3.select(d3.event.relatedTarget);
+		if (rtg.size() && !rtg.classed("mouse") && !rtg.classed("xMouse")) {
+			xAnchor = null;
+		}
+	}
+
+	function xDblclick() {
+		xChanged = false;
+		resetXDomain();
+		redrawXGrid();
+		redrawLines();
+		reload();
+	}
+
+	function yMousedown() {
+		if (settings.yLocked) {
+			return;
+		}
+
+		yAnchor = yScale.invert(d3.mouse(this)[1]);
+		d3.event.preventDefault();
+	}
+
+	function yMouseup() {
+		yAnchor = null;
+	}
+
+	function yMousemove() {
+		var v = yScale.invert(d3.mouse(this)[1]),
+			d = yScale.domain();
+
+		if (yAnchor === null) {
+			return;
+		}
+
+		if (d3.event.shiftKey) {
+			d[1] = d[0] + (d[0] - yAnchor)/(d[0] - v)*(d[1] - d[0]);
+		} else {
+			d[0] += yAnchor - v;
+			d[1] += yAnchor - v;
+		}
+		yScale.domain(d);
+		yChanged = true;
+		redrawYGrid();
+		redrawLines();
+	}
+
+	function yMouseout() {
+		yAnchor = null;
+	}
+
+	function yDblclick() {
+		yChanged = false;
+		resetYDomain();
+		redrawYGrid();
+		redrawLines();
+	}
+	
+	function dblclick() {
+		xChanged = false;
+		resetXDomain();
+		redrawXGrid();
+		yChanged = false;
+		resetYDomain();
+		redrawYGrid();
+		reload();
+	}
+
+	function now() {
+		return new Date((+new Date) + clockSkew);
+	}
+
+	function roundX(d) {
+		return Math.round(xScale(d) - 0.5) + 0.5;
+	}
+
+	function roundY(d) {
+		return Math.round(yScale(d) - 0.5) + 0.5;
+	}
+
+	function formatNumber(x) {
+		var PFX = ["", "k", "M", "G", "T", "P", "E", "Z", "Y"],
+			pfx = ["", "m", "µ", "n", "p", "f", "a", "z", "y"],
+			s = x < 0 ? "-" : "",
+			i = 0, j = 0,
+			a, b, u,
+
+		x = Math.abs(x);
+
+		if (x == 0) {
+			return "0";
+		}
+
+		if (x < 1) {
+			while (x < 1 && i < pfx.length-1) {
+				x *= 1000;
+				i++;
+			}
+			u = pfx[i];
+		} else {
+			while (x >= 1000 && i < PFX.length-1) {
+				x /= 1000;
+				i++;
+			}
+			u = PFX[i];
+		}
+
+		while (Math.round(x) < 1000) {
+			x *= 10;
+			j++;
+		}
+		x = Math.round(x) + "";
+		a = x.substr(0, 4-j);
+		b = x.substr(4-j);
+		for (i = 0; i < b.length; i++) {
+			if (b[i] != "0") {
+				a = a + "." + b;
+				break;
+			}
+		}
+
+		return s + a + u;
+	}
+
+}
+
+
+window.onload = function () {
+	chart = new Chart("#chart", statsd.cache, 0);
+}
---
+++ charts/statsd.js
@@ -0,0 +1,538 @@
+var statsd = function () {
+
+function config(values) {
+	for (var k in config) {
+		if (typeof values[k] != "undefined") {
+			config[k] = values[k];
+		}
+	}
+}
+
+config.httpApi = "/";
+config.wsApi = "/";
+config.timeout = 2000;
+config.cacheFactor = 1;
+
+function archive(sources, from, granularity, length, callback) {
+	var urlPrefix,
+		urls = [],
+		nFields = [],
+		i;
+
+	urlPrefix = config.httpApi +
+		"?type=archive" +
+		"&from=" + encodeURIComponent(from / 1000) +
+		"&length=" + encodeURIComponent(length) +
+		"&granularity=" + encodeURIComponent(granularity / 1000);
+
+	sources = parseSources(sources);
+	for (i = 0; i < sources.length; i++) {
+		urls[i] = urlPrefix +
+			"&metric=" + sources[i].metric +
+			"&channels=" + sources[i].channelsStr;
+		nFields[i] = sources[i].channels.length;
+	}
+
+	fetchData(urls, nFields, httpCallback(sources, callback));
+}
+
+function live(sources, callback) {
+	var urlPrefix,
+		urls = [],
+		nFields = [],
+		i;
+
+	urlPrefix = config.httpApi + "?type=live";
+	sources = parseSources(sources);
+	for (i = 0; i < sources.length; i++) {
+		urls[i] = urlPrefix +
+			"&metric=" + sources[i].metric +
+			"&channels=" + sources[i].channelsStr;
+		nFields[i] = sources[i].channels.length;
+	}
+
+	fetchData(urls, nFields, httpCallback(sources, callback));
+}
+
+function archiveWatch(sources, offset, granularity, callback) {
+	var urlPrefix,
+		urls = [],
+		nFields = [],
+		i;
+
+	urlPrefix = config.wsApi +
+		"?type=archive" +
+		"&offset=" + encodeURIComponent(offset / 1000) +
+		"&granularity=" + encodeURIComponent(granularity / 1000);
+
+	sources = parseSources(sources);
+	for (i = 0; i < sources.length; i++) {
+		urls[i] = urlPrefix +
+			"&metric=" + sources[i].metric +
+			"&channels=" + sources[i].channelsStr;
+		nFields[i] = sources[i].channels.length;
+	}
+
+	return watchData(urls, nFields, wsCallback(sources, callback));
+}
+
+function httpCallback(sources, callback) {
+	return function (error, results) {
+		var merged;
+
+		if (error !== null) {
+			callback(error, null);
+			return;
+		}
+
+		merged = mergeResults(results, sources);
+		if (merged === null) {
+			callback("Invalid response from server", null);
+			return;
+		}
+
+		callback(null, merged);
+	};
+}
+
+function liveWatch(sources, callback) {
+	var urlPrefix,
+		urls = [],
+		nFields = [],
+		i;
+
+	urlPrefix = config.wsApi + "?type=live";
+	sources = parseSources(sources);
+	for (i = 0; i < sources.length; i++) {
+		urls[i] = urlPrefix +
+			"&metric=" + sources[i].metric +
+			"&channels=" + sources[i].channelsStr;
+		nFields[i] = sources[i].channels.length;
+	}
+
+	return watchData(urls, nFields, wsCallback(sources, callback));
+}
+
+function wsCallback(sources, callback) {
+	return function (results) {
+		var merged = mergeResults(results, sources);
+		if (merged === null) {
+			return;
+		}
+		callback(merged);
+	};
+}
+
+function fetchData(urls, nFields, callback) {
+	var responses = [],
+		failed = false,
+		N = 0,
+		i;
+
+	for (i = 0; i < urls.length; i++) {
+		issueRequest(i);
+	}
+
+	if (urls.length == 0) {
+		callback(null, []);
+	}
+
+	function issueRequest(i) {
+		var request;
+
+		try {
+			request = new XMLHttpRequest();
+			request.open("GET", urls[i]);
+			request.timeout = config.timeout;
+			request.onreadystatechange = readyStateChangeHandler;
+			request.send();
+		} catch (ex) {
+			request = new XDomainRequest();
+			request.open("GET", urls[i]);
+			request.timeout = config.timeout;
+			request.ontimeout = function () { error("Server unreachable"); };
+			request.onerror = function () { error("Unknown error"); };
+			request.onload = loadHandler;
+			request.onprogress = function () {};
+			setTimeout(function () { request.send(); }, 0);
+		}
+
+		function readyStateChangeHandler() {
+			if (request.readyState == XMLHttpRequest.DONE) {
+				if (request.status == 0) {
+					error("Server unreachable");
+				} else if (Math.floor(request.status / 100) != 2) {
+					error(request.responseText);
+				} else {
+					loadHandler();
+				}
+			}
+		}
+
+		function loadHandler() {
+			responses[i] = parseResponse(request.responseText, nFields[i] + 1);
+			if (responses[i] === null) {
+				error("Invalid response from server");
+			} else if (++N == urls.length) {
+				callback(null, responses);
+			}
+		}
+
+		function error(msg) {
+			if (failed) {
+				return;
+			}
+			failed = true;
+			callback(msg, null);
+		}
+	}
+}
+
+function watchData(urls, nFields, callback) {
+	var responses = [],
+		sockets = [],
+		maxTs,
+		N = 0,
+		i;
+
+	if (typeof WebSocket == "undefined") {
+		return null;
+	}
+
+	for (i = 0; i < urls.length; i++) {
+		openConnection(i);
+	}
+
+	return close;
+
+	function openConnection(i) {
+		init();
+
+		function init() {
+			sockets[i] = new WebSocket(urls[i]);
+			sockets[i].onmessage = messageHandler;
+			sockets[i].onclose = closeHandler;
+		}
+
+		function messageHandler(ev) {
+			var ts;
+
+			responses[i] = parseResponse(ev.data, nFields[i]+1);
+			if (responses[i] === null || !responses[i][0].length) {
+				return;
+			}
+			ts = responses[i][0][0];
+
+			if (N == 0) {
+				maxTs = ts;
+			}
+			if (ts > maxTs) {
+				maxTs = ts;
+				N = 0;
+			}
+			if (ts == maxTs) {
+				if (++N == urls.length) {
+					N = 0;
+					callback(responses);
+				}
+			}
+		 }
+
+		 function closeHandler(ev) {
+			setTimeout(reinit, config.timeout);
+		 }
+
+		 function reinit() {
+		 	if (sockets[i] !== null) {
+				init();
+			}
+		 }
+	}
+
+	function close() {
+		var i;
+		for (i = 0; i < sockets.length; i++) {
+			if (sockets[i] !== null) {
+				sockets[i].close();
+				sockets[i] = null;
+			}
+		}
+	}
+
+}
+
+function mergeResults(input, sources) {
+	var maxTs = Number.NEGATIVE_INFINITY,
+		minLen = Number.POSITIVE_INFINITY,
+		output = {ts: null, data: []},
+		channel,
+		data,
+		i, j, k;
+
+	if (sources.length == 0) {
+		return output;
+	}
+
+	for (i = 0; i < sources.length; i++) {
+		for (j = 0; j < sources[i].channels.length; j++) {
+			for (k = 0; k < sources[i].channels[j].indices.length; k++) {
+				output.data.push([]);
+			}
+		}
+	}
+
+	for (i = 0; i < input.length; i++) {
+		if (input[i][0].length == 0) {
+			return output;
+		}
+		if (input[i][0][0] > maxTs) {
+			maxTs = input[i][0][0];
+		}
+	}
+
+	for (i = 0; i < input.length; i++) {
+		data = input[i];
+		while (data[0].length > 0 && data[0][0] < maxTs) {
+			for (j = 0; j < data.length; j++) {
+				data[j].shift();
+			}
+		}
+		if (data[0].length < minLen) {
+			minLen = data[0].length;
+		}
+		if (minLen == 0) {
+			return output;
+		}
+		if (data[0][0] != maxTs) {
+			return null;
+		}
+	}
+
+	output.ts = maxTs*1000;
+
+	for (i = 0; i < sources.length; i++) {
+		for (j = 0; j < sources[i].channels.length; j++) {
+			channel = sources[i].channels[j];
+			data = input[i][j+1];
+			data.length = minLen;
+			output.data[channel.indices[0]] = data;
+			for (k = 1; k < channel.indices.length; k++) {
+				output.data[channel.indices[k]] = data.slice();
+			}
+		}
+	}
+
+	return output;
+}
+
+function parseSources(sources) {
+	var output = [],
+		metric,
+		type,
+		channel,
+		obj,
+		i, j, k;
+
+	for (i = 0; i < sources.length; i++) {
+		j = sources[i].indexOf(":");
+		metric = sources[i].substr(0, j);
+		channel = sources[i].substr(j+1).replace(",", "_");
+		j = channel.indexOf("-");
+		type = j == -1 ? channel : channel.substr(0, j);
+
+		for (j = 0; j < output.length; j++) {
+			obj = output[j];
+			if (obj.metric == metric && obj.type == type) {
+				for (k = 0; k < obj.channels.length; k++) {
+					if (obj.channels[k].channel == channel) {
+						obj.channels[k].indices.push(i);
+						break;
+					}
+				}
+				if (k == obj.channels.length) {
+					obj.channels[k] = {channel: channel, indices: [i]};
+					obj.channelsStr += ","+encodeURIComponent(channel);
+				}
+				break;
+			}
+		}
+		if (j == output.length) {
+			output[j] = {
+				metric: metric,
+				type: type,
+				channels: [{channel: channel, indices: [i]}],
+				channelsStr: encodeURIComponent(channel)
+			};
+		}
+	}
+
+	return output;
+}
+
+function parseResponse(csv, nFields) {
+	var 
+		lines = csv.split("\n"),
+		line,
+		field,
+		output = [],
+		i, j;
+
+	if (csv.substr(-1) == "\n") {
+		lines.pop();
+	}
+
+	for (i = 0; i < nFields; i++) {
+		output[i] = [];
+	}
+
+	if (csv.length == 0) {
+		return output;
+	}
+
+	for (i = 0; i < lines.length; i++) {
+		line = lines[i].split(",");
+		if (line.length != nFields) {
+			return null;
+		}
+		for (j = 0; j < nFields; j++) {
+			field = line[j];
+			if (field == "+Inf") {
+				field = Number.MAX_VALUE;
+			} else if (field == "-Inf") {
+				field = -Number.MAX_VALUE;
+			} else if (field == "NaN") {
+				field = 0;
+			} else {
+				field = +field;
+				if (isNaN(field)) {
+					return null;
+				}
+			}
+			output[j][i] = +field;
+		}
+	}
+
+	return output;
+}
+
+function cache(sources, granularity) {
+	var cacheTs,
+		cacheLength,
+		cacheData = null,
+		current = null,
+		next = null;
+
+	function query(from, length, callback) {
+		if (sources.length == 0) {
+			callback(null, {ts: null, data: []});
+		} else {
+			if (needsRefill(from, length)) {
+				refill(from, length, callback);
+			} else {
+				reply(from, length, callback);
+			}
+		}
+	}
+
+	query.watch = function (offset, callback) {
+		if (sources.length == 0) {
+			return null;
+		}
+
+		return archiveWatch(sources, offset, granularity, function (d) {
+			var expected, i;
+
+			if (!needsRefill(d.ts, 1)) {
+				expected = cacheTs + cacheData[0].length*granularity;
+				if (d.ts == expected) {
+					for (i = 0; i < cacheData.length; i++) {
+						cacheData[i].push(d.data[i][0]);
+					}
+				} else if (d.ts > expected) {
+					cacheData = null;
+				}
+			}
+			callback(d.ts);
+		});
+	};
+
+	return query;
+
+	function needsRefill(from, length) {
+		return cacheData === null ||
+			from < cacheTs ||
+			from + length*granularity > cacheTs + cacheLength*granularity;
+	}
+
+	function reply(from, length, callback) {
+		var offset,
+			output;
+	
+		offset = (from - cacheTs) / granularity;
+		output = {
+			ts: null,
+			data: cacheData.map(function (d) {
+				return d.slice(offset, offset + length);
+			})
+		};
+		if (output.data.length != 0) {
+			output.ts = from;
+		}
+		
+		callback(null, output);
+	}
+
+	function refill(from, length, callback) {
+		var cf = Math.max(0, config.cacheFactor),
+			qFrom = from - granularity*Math.round(length * cf),
+			qLength = Math.round(length * (1 + 2*cf)),
+			q = {
+				from: from,
+				length: length,
+				callback: callback,
+				qFrom: qFrom,
+				qLength: qLength};
+	
+		if (current) {
+			next = q;
+		} else {
+			current = q;
+			archive(sources, qFrom, granularity, qLength, refillCallback);
+		}
+	}
+
+	function refillCallback(error, data) {
+		var c = current,
+			n = next;
+
+		current = next = null;
+
+		if (error === null) {
+			cacheTs = c.qFrom;
+			cacheLength = c.qLength;
+			cacheData = data.data;
+		}
+
+		if (n) {
+			query(n.from, n.length, n.callback);
+		} else {
+			if (error === null) {
+				reply(c.from, c.length, c.callback);
+			} else {
+				c.callback.call(this, error, null);
+			}
+		}
+	}
+
+}
+
+return {
+	config: config,
+	archive: archive,
+	archiveWatch: archiveWatch,
+	live: live,
+	liveWatch: liveWatch,
+	cache: cache
+};
+
+}();
